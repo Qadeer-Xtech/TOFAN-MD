1713d50f89fce25881e9d195017087bc319bcfd5
Qadeer-bhai <ummerkulachi@gmail.com>
2025-06-20 14:57:50 +0500

    Update ping.js

--- tofancmd/ping.js
+++ tofancmd/ping.js
@@ -1,11 +1,17 @@
-const { ezra } = require("./../fredi/ezra");
-const { format, runtime } = require('../fredi/mesfonctions');
+const {
+  ezra
+} = require("./../fredi/ezra");
+const {
+  format,
+  runtime
+} = require('../fredi/mesfonctions');
 const os = require('os');
 const speed = require('performance-now');
-const { performance } = require('perf_hooks');
+const {
+  performance
+} = require('perf_hooks');
 const conf = require('../set');
 
-// 🔹 PING Command
 ezra(
   {
     nomCom: 'ping',
@@ -13,123 +19,108 @@ ezra(
     reaction: '🚀',
     alias: ['p']
   },
+
   async (dest, zk, commandOptions) => {
-    const { ms } = commandOptions;
+    const {
+      ms, arg, repondre
+    } = commandOptions;
     const start = new Date().getTime();
     const msg = await zk.sendMessage(dest, {
       text: '*Testing Ping!!!*',
-      contextInfo: {
-        forwardingScore: 999,
-        isForwarded: true,
-        forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363345872435489@newsletter',
-          newsletterName: conf.BOT || "TOFAN-MD",
-          serverMessageId: 143
-        }
-      }
-    }, { quoted: ms });
-
+    }, {
+      quoted: ms
+    });
     const end = new Date().getTime();
     const ping = end - start;
-
     await zk.sendMessage(dest, {
-      text: `*Pong*\n *${ping} ms*`,
-      edit: { id: msg.key.id, remoteJid: dest },
-      contextInfo: {
-        forwardingScore: 999,
-        isForwarded: true,
-        forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363345872435489@newsletter',
-          newsletterName: conf.BOT || "TOFAN-MD",
-          serverMessageId: 143
-        }
-      }
-    });
-
+      text: `*Pong*
+ *${ping} ms*`, edit: {
+        id: msg.key.id, remoteJid: dest
+      }});
     await zk.sendMessage(dest, {
       react: {
-        text: "⚙️",
-        key: ms.key
-      }
-    });
+        text: "⚙️", key: ms.key
+      }})
   }
-);
+)
 
-// 🔹 DESC Command
 ezra(
   {
     nomCom: 'desc',
     reaction: 'ℹ',
     alias: ['i']
   },
+
   async (dest, zk, commandOptions) => {
-    const { ms } = commandOptions;
+    const {
+      ms, arg, repondre
+    } = commandOptions;
+    // data
     const tumbUrl = 'https://qu.ax/NfJZk.jpg';
     const used = process.memoryUsage();
     const cpus = os.cpus().map(cpu => {
       cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0);
-      return cpu;
+      return cpu
     });
-    const cpu = cpus.reduce((last, cpu, _, { length }) => {
-      last.total += cpu.total;
-      last.speed += cpu.speed / length;
-      last.times.user += cpu.times.user;
-      last.times.nice += cpu.times.nice;
-      last.times.sys += cpu.times.sys;
-      last.times.idle += cpu.times.idle;
-      last.times.irq += cpu.times.irq;
-      return last;
+    const cpu = cpus.reduce((last, cpu, _, {
+      length
+    }) => {
+      last.total += cpu.total
+      last.speed += cpu.speed / length
+      last.times.user += cpu.times.user
+      last.times.nice += cpu.times.nice
+      last.times.sys += cpu.times.sys
+      last.times.idle += cpu.times.idle
+      last.times.irq += cpu.times.irq
+      return last
     }, {
       speed: 0,
       total: 0,
-      times: { user: 0, nice: 0, sys: 0, idle: 0, irq: 0 }
+      times: {
+        user: 0,
+        nice: 0,
+        sys: 0,
+        idle: 0,
+        irq: 0
+      }
     });
-
     let timestamp = speed();
     let latensi = speed() - timestamp;
     let neww = performance.now();
     let oldd = performance.now();
-
     const response = `
 Response Speed ${latensi.toFixed(4)} _Second_ \n ${oldd - neww} _miliseconds_\n\nRuntime : ${runtime(process.uptime())}
 
 💻 Info Server
     RAM: ${format(os.totalmem() - os.freemem())} / ${format(os.totalmem())}
 
-_NodeJS Memory Usage_
+_NodeJS Memory Usaage_
     ${Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n')}
 
 ${cpus[0] ? `_Total CPU Usage_
     ${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}
 _CPU Core(s) Usage (${cpus.length} Core CPU)_
-    ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
+    ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}`: ''}
     `.trim();
-
     await zk.sendMessage(dest, {
       text: response,
       contextInfo: {
-        forwardingScore: 999,
-        isForwarded: true,
-        forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363345872435489@newsletter',
-          newsletterName: conf.BOT || "TOFAN-MD",
-          serverMessageId: 143
-        },
         externalAdReply: {
           showAdAttribution: true,
           title: `${conf.BOT}`,
           body: `${latensi.toFixed(4)} Second`,
-          thumbnailUrl: tumbUrl,
+          thumbnailUrl: `${tumbUrl}`,
           sourceUrl: global.link,
           mediaType: 1,
-          renderLargerThumbnail: true
+          renderLargerAbhinail: true
         }
       }
-    }, { quoted: ms });
+    }, {
+      quoted: ms
+    })
   }
 );
 
-// 🔹 RUNTIME Command
 ezra(
   {
     nomCom: 'runtime',
@@ -137,20 +128,14 @@ ezra(
     alias: ['uptime']
   },
   async (dest, zk, commandOptions) => {
-    const { ms } = commandOptions;
+    const {
+      ms
+    } = commandOptions;
     const tumbUrl = 'https://qu.ax/NfJZk.jpg';
     const runtimetext = `🚨 *Bot Have Been Running For ${runtime(process.uptime())}* 🚨`;
-
-    await zk.sendMessage(dest, {
+    zk.sendMessage(dest, {
       text: runtimetext,
       contextInfo: {
-        forwardingScore: 999,
-        isForwarded: true,
-        forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363345872435489@newsletter',
-          newsletterName: conf.BOT || "TOFAN-MD",
-          serverMessageId: 143
-        },
         externalAdReply: {
           showAdAttribution: true,
           title: `${conf.BOT}`,
@@ -158,9 +143,12 @@ ezra(
           thumbnailUrl: tumbUrl,
           sourceUrl: global.link,
           mediaType: 1,
-          renderLargerThumbnail: true
+          renderLargerAbhinail: true
         }
       }
-    }, { quoted: ms });
+    }, {
+      quoted: ms
+    })
   }
 );
+    
