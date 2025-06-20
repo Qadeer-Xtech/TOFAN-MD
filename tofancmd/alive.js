f2996e3b6fdc8e3c33e524f0de5b51fc719b3405
Qadeer-bhai <ummerkulachi@gmail.com>
2025-06-20 20:04:52 +0500

    Update alive.js

--- tofancmd/alive.js
+++ tofancmd/alive.js
@@ -1,87 +1,97 @@
 const { ezra } = require('../fredi/ezra');
-const { addOrUpdateDataInAlive, getDataFromAlive } = require('../tofandatabase/alive');
+const {addOrUpdateDataInAlive , getDataFromAlive} = require('../tofandatabase/alive')
 const moment = require("moment-timezone");
 const s = require(__dirname + "/../set");
 
 ezra(
-  {
-    nomCom: 'live-me',
-    categorie: 'General'
-  },
-  async (dest, zk, commandeOptions) => {
+    {
+        nomCom : 'live-me',
+        categorie : 'General'
+        
+    },async (dest,zk,commandeOptions) => {
 
-    const { ms, arg, repondre, superUser } = commandeOptions;
-    const data = await getDataFromAlive();
+ const {ms , arg, repondre,superUser} = commandeOptions;
 
-    if (!arg || !arg[0] || arg.join('') === '') {
+ const data = await getDataFromAlive();
 
-      if (data) {
+ if (!arg || !arg[0] || arg.join('') === '') {
 
-        const { message, lien } = data;
+    if(data) {
+       
+        const {message , lien} = data;
+
 
         var mode = "public";
         if ((s.MODE).toLocaleLowerCase() != "yes") {
-          mode = "private";
+            mode = "private";
         }
+      
+    
+     
+    moment.tz.setDefault('Etc/PKT');
 
-        moment.tz.setDefault('Etc/PKT');
-        const temps = moment().format('HH:mm:ss');
-        const date = moment().format('DD/MM/YYYY');
+// Créer une date et une heure en PKT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
 
-        const alivemsg = `
+    const alivemsg = `
 *Owner* : ${s.OWNER_NAME}
 *Mode* : ${mode}
 *Date* : ${date}
-*Hours(PKT)* : ${temps}
-*From* : PAKISTAN
+*Hours(GMT)* : ${temps}
+*From* : Pakistan
 *Bot* : ${s.bot} 
+*Forks* : ${forks} 
 
-${message}
+ ${message}
+ 
+ 
+ *TOFAN MD 2025*`
 
-*TOFAN MD 2025*`;
-
-        if (lien.match(/\.(mp4|gif)$/i)) {
-          try {
-            await zk.sendMessage(dest, { video: { url: lien }, caption: alivemsg }, { quoted: ms });
-          } catch (e) {
-            console.log("🥵🥵 Menu erreur " + e);
-            repondre("🥵🥵 Menu erreur " + e);
-          }
-        }
-        else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
-          try {
-            await zk.sendMessage(dest, { image: { url: lien }, caption: alivemsg }, { quoted: ms });
-          } catch (e) {
-            console.log("🥵🥵 Menu erreur " + e);
-            repondre("🥵🥵 Menu erreur " + e);
-          }
-        }
-        else {
-          repondre(alivemsg);
-        }
-
-      } else {
-        if (!superUser) {
-          repondre("Hello Friend Tofan Md Is A Live 24hrs Enjoy To Use All Time🤗");
-          return;
-        }
-
-        await repondre("You have not yet saved your alive, to do this; enter after alive your message and your image or video link in this context: .alive message;lien");
-        repondre("don't do fake thinks :)");
-      }
+ if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption: alivemsg }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Checking for .jpeg or .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption: alivemsg }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    
+    repondre(alivemsg);
+    
+}
 
     } else {
+        if(!superUser) { repondre("Hello Tofan Md Is A Live 24hrs Enjoy To Use All Time🤗") ; return};
 
-      if (!superUser) {
-        repondre("Uuuhh Only the owner can modify the alive");
-        return;
-      }
+      await   repondre("You have not yet saved your alive, to do this;  enter after alive your message and your image or video link in this context: .alive message;lien");
+         repondre("don't do fake thinks :)")
+     }
+ } else {
 
-      const texte = arg.join(' ').split(';')[0];
-      const tlien = arg.join(' ').split(';')[1];
+    if(!superUser) { repondre ("Uuuhh Only the owner can  modify the alive") ; return};
 
-      await addOrUpdateDataInAlive(texte, tlien);
-      repondre('Tofan Md Is A Live 24hrs Enjoy To Use All Time🤗.');
-    }
-  }
-);
+  
+    const texte = arg.join(' ').split(';')[0];
+    const tlien = arg.join(' ').split(';')[1]; 
+
+
+    
+await addOrUpdateDataInAlive(texte , tlien)
+
+repondre('  Tofan Md Is A Live 24hrs Enjoy To Use All Time🤗. ')
+
+}
+    });
